8fd65fe1f8ba126583442a0d3d3ecb7ba2a3001e
asthashukla106 <221b106@juetguna.in>
2025-04-18 10:34:07 +0530

    Add files via upload

---
+++ LE 8/IndianCrow.java
@@ -0,0 +1,14 @@
+public class IndianCrow implements Crow{
+    public void cry()
+    {
+        System.out.println("crow cries");
+    }
+    public void fly()
+    {
+        System.out.println("fly high over mountains");
+    }
+    public void eat()
+    {
+        System.out.println("eats perals");
+    }
+}
